ba94d8719a23b03abb76d097a2defa68f56bcc76
Harish Kannan <69982900+harishkannan05@users.noreply.github.com>
2025-03-27 17:38:03 -0400

    Add files via upload

---
+++ Attachments/pwn21.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_banner(){
+	printf( "  ______ _          __      __         _ _   \n"
+ 		" |  ____| |         \\ \\    / /        | | |  \n"
+		" | |__  | | __ _  __ \\ \\  / /_ _ _   _| | |_ \n"
+		" |  __| | |/ _` |/ _` \\ \\/ / _` | | | | | __|\n"
+		" | |    | | (_| | (_| |\\  / (_| | |_| | | |_ \n"
+		" |_|    |_|\\__,_|\\__, | \\/ \\__,_|\\__,_|_|\\__|\n"
+		"                  __/ |                      \n"
+		"                 |___/                       \n"
+		"                                             \n"
+		"Version 2.1 - Fixed print_flag to not print the flag. Nothing you can do about it!\n"
+		"==================================================================\n\n"
+	      );
+}
+
+void print_flag(char *username){
+        FILE *f = fopen("flag.txt","r");
+        char flag[200];
+
+        fgets(flag, 199, f);
+        //printf("%s", flag);
+	
+	//The user needs to be mocked for thinking they could retrieve the flag
+	printf("Hello, ");
+	printf(username);
+	printf(". Was version 2.0 too simple for you? Well I don't see no flags being shown now xD xD xD...\n\n");
+	printf("Yours truly,\nByteReaper\n\n");
+}
+
+void login(){
+	char username[100] = "";
+
+	printf("Username: ");
+	gets(username);
+
+	// The flag isn't printed anymore. No need for authentication
+	print_flag(username);
+}
+
+void main(){
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	// Start login process
+	print_banner();
+	login();
+
+	return;
+}
